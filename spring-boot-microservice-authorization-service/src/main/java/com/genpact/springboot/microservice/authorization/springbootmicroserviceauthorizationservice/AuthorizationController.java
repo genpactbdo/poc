0153dd683541f9fc82481d41bd543036e548db48
shakeys <shakeys@Shakeys>
2018-03-14 13:43:10 +0800

    rest authorize call

--- spring-boot-microservice-authorization-service/src/main/java/com/genpact/springboot/microservice/authorization/springbootmicroserviceauthorizationservice/AuthorizationController.java
+++ spring-boot-microservice-authorization-service/src/main/java/com/genpact/springboot/microservice/authorization/springbootmicroserviceauthorizationservice/AuthorizationController.java
@@ -23,10 +23,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.genpact.springboot.microservice.authorization.springbootmicroserviceauthorizationservice.model.Remittance;
