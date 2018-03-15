d66164672d480c1a947cd38cce5291e8145f0eb3
shakeys <shakeys@Shakeys>
2018-03-15 14:30:51 +0800

    Add conduit and conduit page and update

--- spring-boot-microservice-authorization-service/src/main/java/com/genpact/springboot/microservice/authorization/springbootmicroserviceauthorizationservice/model/Remittance.java
+++ spring-boot-microservice-authorization-service/src/main/java/com/genpact/springboot/microservice/authorization/springbootmicroserviceauthorizationservice/model/Remittance.java
@@ -13,7 +13,6 @@ public class Remittance {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
  
-    
     private String status;
     private String conduit;
     private Date date;
