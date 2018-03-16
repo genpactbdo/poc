29510673c4a37ee2d123c1955f32ce7886c1e93f
shakeys <shakeys@Shakeys>
2018-03-16 13:44:30 +0800

    Update data

--- spring-boot-microservice-authorization-service/src/main/resources/data.sql
+++ spring-boot-microservice-authorization-service/src/main/resources/data.sql
@@ -1,13 +1,5 @@
 insert into remittance(id,conduit,status,source_Amount) values (1,'ML','O',5000);
-insert into remittance(id,conduit,status,source_Amount) values (2,'ML','P',5000);
-insert into remittance(id,conduit,status,source_Amount) values (3,'SB','O',8000);
-insert into remittance(id,conduit,status,source_Amount) values (4,'ML','P',3000);
-insert into remittance(id,conduit,status,source_Amount) values (5,'ML','P',9000);
-insert into remittance(id,conduit,status,source_Amount) values (6,'ML','P',8000);
-insert into remittance(id,conduit,status,source_Amount) values (7,'SB','O',6000);
-insert into remittance(id,conduit,status,source_Amount) values (8,'ML','P',7000);
-insert into remittance(id,conduit,status,source_Amount) values (9,'ML','O',2000);
-insert into remittance(id,conduit,status,source_Amount) values (10,'ML','O',3000);
 
 insert into conduit(conduit_name, balance, credit_limit, total_remit) values ('ML', 2000, 10000, 0);
 insert into conduit(conduit_name, balance, credit_limit, total_remit) values ('SB', 1000, 10000, 0);
+insert into conduit(conduit_name, balance, credit_limit, total_remit) values ('BDO Remit (USA) Inc.', 5000, 100000, 0);
